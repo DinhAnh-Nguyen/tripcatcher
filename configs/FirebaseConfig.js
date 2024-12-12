f992175bc7bbe1a446ed3358a079353672924e2a
DinhAnh2301 <dinhanh2301@gmail.com>
2024-12-12 11:51:03 -0700

    updated

--- configs/FirebaseConfig.js
+++ configs/FirebaseConfig.js
@@ -6,19 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 
-  apiKey: "AIzaSyDF0OSGyuvi_uj03xChzSoJnuZSjSErq0U",
-  authDomain: "tripcatcher-cab7b.firebaseapp.com",
-  projectId: "tripcatcher-cab7b",
-  storageBucket: "tripcatcher-cab7b.firebasestorage.app",
-  messagingSenderId: "838735976094",
-  appId: "1:838735976094:web:dbeab955a3143ea58a8d8f"
 };
 
 // Initialize Firebase
