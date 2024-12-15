85e793c19d06f59742280d96243f4906c9460051
bnhao <88442790+bnhao@users.noreply.github.com>
2024-12-14 17:02:31 -0700

    udate

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,21 @@
-// Managing the main layout of the app
-
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import React from 'react';
 
 export default function RootLayout() {
+  useFonts({
+    'outfit': require('./../assets/fonts/Outfit-Regular.ttf'),
+    'outfit-medium': require('./../assets/fonts/Outfit-Medium.ttf'),
+    'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf')
+  })
+
   return (
-    // Hide the screen header for every screens inside this Stack component
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="index" />
+    <Stack>
+      {/* <Stack.Screen name="index" option={{
+        headerShown:false
+      }} /> */}
+      <Stack.Screen name="(tabs)" />
+
     </Stack>
   );
 }
