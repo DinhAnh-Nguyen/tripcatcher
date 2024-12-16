547fdbf18350b65a1979500e63b04362432aa981
bnhao <88442790+bnhao@users.noreply.github.com>
2024-12-16 16:57:17 -0700

    update

--- app/create-trip/review-trip.jsx
+++ app/create-trip/review-trip.jsx
@@ -1,10 +1,13 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React, { useEffect } from "react";
-import { useNavigation, useRouter } from "expo-router";
-import { useContext } from "react";
-import { CreateTripContext } from "./../../context/CreateTripContext";
-import moment from "moment";
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, { useEffect } from 'react'
+import { useNavigation, useRouter } from 'expo-router'
+import { Colors } from './../../constants/Colors'
+import { Ionicons } from '@expo/vector-icons';
+import { useContext } from 'react';
+import { CreateTripContext } from './../../context/CreateTripContext'
+import moment from 'moment'
 export default function ReviewTrip() {
+
   const navigation = useNavigation();
   const { tripData, setTripData } = useContext(CreateTripContext);
 
@@ -13,66 +16,155 @@ export default function ReviewTrip() {
     navigation.setOptions({
       headerShown: true,
       headerTransparent: true,
-      headerTitle: "",
-    });
-  }, []);
+      headerTitle: ''
+    })
+  }, [])
 
   // 📍📆🗓️🚌💰
   return (
-    <View>
-      <Text>Review your trip</Text>
+    <View style={{
+      padding: 25,
+      paddingTop: 75,
+      backgroundColor: Colors.WHITE,
+      height: '100%'
+    }}>
+      <Text style={{
+        fontFamily: 'outfit-bold',
+        fontSize: 35,
+        marginTop: 20
+      }}>Review your trip</Text>
 
-      <View>
-        <Text>Before generating your trip , please review your selection</Text>
+      <View style={{
+        marginTop: 20
+      }}>
+        <Text style={{
+          fontFamily: 'outfit-bold',
+          fontSize: 20
+        }}>Before generating your trip , please review your selection</Text>
 
         {/* Destination Info  */}
-        <View>
-          <Text>📍</Text>
+        <View style={{
+          marginTop: 40,
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 20
+        }}>
+          <Text style={{
+            fontSize: 30
+          }}>📍</Text>
           <View>
-            <Text>Destination</Text>
-            <Text>{tripData?.locationInfo?.name}</Text>
+            <Text style={{
+              fontFamily: 'outfit',
+              fontSize: 17,
+              color: Colors.GRAY
+            }}>Destination</Text>
+            <Text style={{
+              fontFamily: 'outfit-medium',
+              fontSize: 20
+            }}>{tripData?.locationInfo?.name}</Text>
+
           </View>
         </View>
 
         {/* Date Selected Info  */}
-        <View>
-          <Text>🗓️</Text>
+        <View style={{
+          marginTop: 25,
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 20
+        }}>
+          <Text style={{
+            fontSize: 30
+          }}>🗓️</Text>
           <View>
-            <Text>Travel Date</Text>
-            <Text>
-              {moment(tripData?.startDate).format("DD MMM") +
-                " To " +
-                moment(tripData.endDate).format("DD MMM") +
-                "   "}
+            <Text style={{
+              fontFamily: 'outfit',
+              fontSize: 17,
+              color: Colors.GRAY
+            }}>Travel Date</Text>
+            <Text style={{
+              fontFamily: 'outfit-medium',
+              fontSize: 20
+            }}>{moment(tripData?.startDate).format('DD MMM')
+              + " To " +
+              moment(tripData.endDate).format('DD MMM') + "   "}
               ({tripData?.totalNoOfDays} days)
             </Text>
+
           </View>
         </View>
 
-        {/* Travel Info  */}
-        <View>
-          <Text>🚌</Text>
+        {/* Traveles Info  */}
+        <View style={{
+          marginTop: 25,
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 20
+        }}>
+          <Text style={{
+            fontSize: 30
+          }}>🚌</Text>
           <View>
-            <Text>Who is Traveling</Text>
-            <Text>{tripData?.traveler?.title}</Text>
+            <Text style={{
+              fontFamily: 'outfit',
+              fontSize: 17,
+              color: Colors.GRAY
+            }}>Who is Traveling</Text>
+            <Text style={{
+              fontFamily: 'outfit-medium',
+              fontSize: 20
+            }}>{tripData?.traveler?.title}
+            </Text>
+
           </View>
         </View>
 
         {/* Budget Info  */}
-        <View>
-          <Text>💰</Text>
+        <View style={{
+          marginTop: 25,
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 20
+        }}>
+          <Text style={{
+            fontSize: 30
+          }}>💰</Text>
           <View>
-            <Text>Budget</Text>
-            <Text>{tripData?.budget}</Text>
+            <Text style={{
+              fontFamily: 'outfit',
+              fontSize: 17,
+              color: Colors.GRAY
+            }}>Budget</Text>
+            <Text style={{
+              fontFamily: 'outfit-medium',
+              fontSize: 20
+            }}>{tripData?.budget}
+            </Text>
+
           </View>
         </View>
+
       </View>
 
       <TouchableOpacity
-        onPress={() => router.replace("/create-trip/generate-trip")}
-      >
-        <Text>Build My trip</Text>
+        onPress={() => router.replace('/create-trip/generate-trip')}
+        style={{
+          padding: 15,
+          backgroundColor: Colors.PRIMARY,
+          borderRadius: 15,
+          marginTop: 80
+        }}>
+
+        <Text style={{
+          textAlign: 'center',
+          color: Colors.WHITE,
+          fontFamily: 'outfit-medium',
+          fontSize: 20,
+        }}>
+          Build My trip</Text>
+
       </TouchableOpacity>
+
     </View>
-  );
+  )
 }
--- app/create-trip/search-place.jsx
+++ app/create-trip/search-place.jsx
@@ -1,23 +1,41 @@
-import { View, Text } from "react-native";
-import React, { useContext, useEffect } from "react";
-import { useRouter } from "expo-router";
-import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
-import { CreateTripContext } from "./../../context/CreateTripContext";
+import { View, Text } from 'react-native'
+import React, { useContext, useEffect } from 'react'
+import { useNavigation, useRouter } from 'expo-router'
+import { Colors } from '../../constants/Colors';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { CreateTripContext } from './../../context/CreateTripContext'
 export default function SearchPlace() {
+
+  const navigation = useNavigation();
   const { tripData, setTripData } = useContext(CreateTripContext);
   const router = useRouter();
+  useEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+      headerTransparent: true,
+      headerTitle: 'Search'
+    })
+  }, []);
 
   useEffect(() => {
     console.log(tripData);
-  }),
-    [tripData];
+  }), [tripData]
 
   return (
-    <View>
+    <View
+      style={{
+        padding: 25,
+        paddingTop: 75,
+        backgroundColor: Colors.WHITE,
+        height: '100%'
+      }}
+    >
+
+
       <GooglePlacesAutocomplete
-        placeholder="Search Place"
+        placeholder='Search Place'
         fetchDetails={true}
-        onFail={(error) => console.log(error)}
+        onFail={error => console.log(error)}
         onPress={(data, details = null) => {
           // 'details' is provided when fetchDetails = true
           setTripData({
@@ -25,17 +43,28 @@ export default function SearchPlace() {
               name: data.description,
               coordinates: details?.geometry.location,
               photoRef: details?.photos[0]?.photo_reference,
-              url: details?.url,
-            },
+              url: details?.url
+            }
           });
 
-          router.push("/create-trip/select-traveler");
+          router.push('/create-trip/select-traveler')
+
         }}
         query={{
           key: process.env.EXPO_PUBLIC_GOOGLE_MAP_KEY,
-          language: "en",
+          language: 'en',
         }}
+        styles={{
+          textInputContainer: {
+            borderWidth: 1,
+            borderRadius: 5,
+            marginTop: 25
+          }
+        }}
+
+
       />
+
     </View>
-  );
+  )
 }
