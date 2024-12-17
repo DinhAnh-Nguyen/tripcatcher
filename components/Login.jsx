005991789771241c25288e94adc460a7e3792b69
bnhao <88442790+bnhao@users.noreply.github.com>
2024-12-16 17:08:02 -0700

    Update Login.jsx

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,30 +1,64 @@
-// Landing page with SignIn / SignUp option
-
-import { View, Button } from "react-native";
-import React from "react";
-import { useRouter } from "expo-router";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Colors } from '@/constants/Colors'
+import { useRouter } from 'expo-router'
 
 export default function Login() {
-  const router = useRouter();
 
+  const router = useRouter();
   return (
     <View>
-      <View>
-        {/* Clicking this button leads you to the auth/sign-in route */}
-        <Button
-          onPress={() => router.push("auth/sign-in")}
-          title="Sign in"
-          accessibilityLabel="Sign In Button"
-        />
-      </View>
-      <View>
-        {/* Clicking this button leads you to the auth/sign-up route */}
-        <Button
-          onPress={() => router.push("auth/sign-up")}
-          title="Create new account"
-          accessibilityLabel="Create New Account Button"
-        />
+      <Image
+        style={{
+          width: '100%',
+          height: 520
+        }}
+      />
+      <View style={styles.container}>
+        <Text style={{
+          fontSize: 30,
+          fontFamily: 'outfit-bold',
+          textAlign: 'center',
+          marginTop: 10
+        }}>AI Travel Planner</Text>
+
+        <Text style={{
+          fontFamily: 'outfit',
+          fontSize: 17,
+          textAlign: 'center',
+          color: Colors.GRAY,
+          marginTop: 20
+        }}>Discover your next adventure effortlessly. Personalized itineraries at your fingertips. Travel smarter with AI-driven insights."</Text>
+
+        <TouchableOpacity style={styles.button}
+          onPress={() => router.push('auth/sign-in')}
+        >
+          <Text style={{
+            color: Colors.WHITE,
+            textAlign: 'center',
+            fontFamily: 'outfit',
+            fontSize: 17
+          }}>Get Started</Text>
+        </TouchableOpacity>
+
       </View>
     </View>
-  );
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.WHITE,
+    marginTop: -20,
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    height: '100%',
+    padding: 25
+  },
+  button: {
+    padding: 15,
+    backgroundColor: Colors.PRIMARY,
+    borderRadius: 99,
+    marginTop: '20%'
+  }
+})
